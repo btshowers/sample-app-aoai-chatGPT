a8e6ee6da18051720006a283c251e380159d1d0e
Brandon Showers <btshowers@gmail.com>
2024-02-09 12:49:55 -0700

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -642,8 +642,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Brandon's Tunes Chat</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot uses my private mp3 collection and associated metadata; Ask me anything about my music.</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px" }} role="log">
